25dc5e56a0db20071f14a8607ca7659e2aa44fb7
Dmitry Roar <dmitrydima1488@yandex.ru>
2020-12-26 00:11:05 +0700

    [screen code] todos ui..

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit {
   form: FormGroup
   todos: ITodos[] | [] = []
 
+  btnDisabled: boolean
+
   constructor(
     private readonly appService: AppService
   ) {
@@ -33,13 +35,27 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit(): void {
+    const title = this.form.value.title as string
+
+    if (title) {
+      this.btnDisabled = false
+    }
+
     const data: ITodos = {
-      title: this.form.value.title,
+      title,
       date: new Date().toJSON(),
       confirm: false
     }
+    this.btnDisabled = true
 
-    this.appService.create(data).subscribe()
+    this.appService.create(data).subscribe(() => {
+    }, () => {
+      this.btnDisabled = false
+      this.form.reset()
+    }, () => {
+      this.btnDisabled = false
+      this.form.reset()
+    })
     this.loadTodos()
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,8 +3,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http'
 import {NgModule} from '@angular/core'
 
-import {AppComponent} from './app.component';
-import { TodosComponent } from './todos/todos.component'
+import {AppComponent} from './app.component'
+import {TodosComponent} from './todos/todos.component'
+
+import {registerLocaleData} from '@angular/common'
+import ruLocale from '@angular/common/locales/ru'
+registerLocaleData(ruLocale)
 
 @NgModule({
   declarations: [
